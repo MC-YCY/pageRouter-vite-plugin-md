4ded2df671a1cfb211cddac55cff9afe9505f295
尹春阳 <2646403766@qq.com>
2023-12-29 17:28:49 +0800

    feat: mds.ts

--- src/router/mds.ts
+++ src/router/mds.ts
@@ -140,5 +140,284 @@ export const Study:any = [
                 ]
             }
         ]
+    },
+    {
+        name:'精通React',
+        children:[
+            {name:'深入理解 JSX：从零开始实现一个 JSX 解析器'},
+            {name:'React实践'},
+            {name:'React18如何提高性能？'},
+            {name:'探索主流前端框架的响应式原理'},
+            {name:'React api和代码重用的演变'},
+            {name:'2023年的React生态'},
+            {name:'2023年如何搭建新的React项目'},
+            {name:'React常见错误，如何避免?'},
+            {name:'React渲染的未来'},
+            {name:'React Router'},
+            {name:'如何在React中应用SOLID原则'},
+            {name:'如何使用React Hook重构类组件'},
+            {name:'如何组织 React 组件代码结构'},
+            {name:'在 React 中实现条件渲染的7种方式'},
+            {name:'React + TypeScript：如何处理常见事件'},
+            {name:'React 中的浅比较是如何工作的'},
+            {name:'如何编写干净的 React 代码'},
+            {name:'如何编写高性能的 React 代码：规则、模式、注意事项'},
+            {name:'如何优雅地在 React 中使用TypeScript'},
+            {name:'React 中五种常见的样式策略'},
+            {name:'使用 React Hooks 时要避免的6个错误'},
+            {name:'React Hooks 初探'},
+            {name:'React 基础入门'},
+            {name:'React 组件实践'},
+            {name:'React 组件化开发'},
+            {name:'React 生命周期'},
+            {name:'React 组件通信'},
+            {name:'React 纯函数组件'},
+            {name:'React 组件设计模式'},
+            {name:'React 渲染流程'},
+        ]
+    },
+    {
+        name:'TypeScript',
+        children:[
+            {
+                name:'基础篇',
+                children:[
+                    {name:'简史'},
+                    {name:'快速入门'},
+                    {name:'数据类型'},
+                    {name:'枚举类型'},
+                    {name:'函数类型'},
+                    {name:'类类型'},
+                    {name:'接口类型'},
+                ]
+            },
+            {
+                name:'进阶篇',
+                children:[
+                    {name:'泛型'},
+                    {name:'装饰器'},
+                    {name:'严格模式'},
+                    {name:'工具类型'},
+                    {name:'映射类型'},
+                    {name:'高级类型'},
+                    {name:'tsconfig'},
+                    {name:'JS迁移TS'},
+                    {name:'标准库类型'},
+                    {name:'类型兼容性'},
+                ]
+            }
+        ]
+    },
+    {
+        name:'前端工程化',
+        children:[
+            {name:'Monorepo'},
+            {name:'VS Code技巧'},
+            {name:'在 Git 中撤消更改的 6 种方法'},
+            {name:'如何配置 Git 提交代码检查'},
+            {name:'学习 Git，看这一篇就够了'},
+            {name:'SPA vs MPA vs PWA'},
+            {name:'package.json'},
+            {name:'常见的编程方式'},
+            {name:'前端模块化规范'},
+            {name:'前端发展和演进史'},
+            {name:'邂逅Webpack'},
+        ]
+    },
+    {
+        name:'浏览器原理',
+        children:[
+            {name:'前端安全'},
+            {name:'浏览器存储'},
+            {name:'进程与线程'},
+            {name:'浏览器事件机制'},
+            {name:'同源策略和跨域'},
+            {name:'浏览器渲染原理'},
+        ]
+    },
+    {
+        name:'计算机网络',
+        children:[
+            {name:'DNS'},
+            {name:'网络模型'},
+            {name:'TCP与UDP'},
+            {name:'Websocket'},
+            {name:'HTTP概述'},
+            {name:'HTTPS概述'},
+            {name:'HTTP状态码'},
+        ]
+    },
+    {
+        name:'前端设计模式',
+        children:[
+            {name:'前端设计模式'},
+            {name:'单例模式'},
+            {name:'工厂模式'},
+            {name:'状态模式'},
+            {name:'策略模式'},
+            {name:'代理模式'},
+            {name:'装饰器模式'},
+            {name:'适配器模式'},
+            {name:'迭代器模式'},
+            {name:'观察者模式/发布-订阅模式'},
+        ]
+    },
+    {
+        name:'前端性能优化',
+        children:[
+            {name:'什么是性能优化'},
+            {name:'图像优化'},
+            {name:'加载优化'},
+            {name:'雅虎35条军规'},
+        ]
+    },
+    {
+        name:'前端宝藏',
+        children:[
+            {
+                name:'开源库',
+                children:[
+                    {name:'词云'},
+                    {name:'3D 库'},
+                    {name:'甘特图'},
+                    {name:'国际化'},
+                    {name:'测试库'},
+                    {name:'图标库'},
+                    {name:'动画库'},
+                    {name:'Vue 库'},
+                    {name:'React 库'},
+                    {name:'代码高亮'},
+                    {name:'快捷键库'},
+                    {name:'产品引导'},
+                    {name:'数据处理'},
+                    {name:'视频播放器'},
+                    {name:'拖拽排序库'},
+                    {name:'文件处理库'},
+                    {name:'Node.js 框架'},
+                    {name:'前端代码规范'},
+                    {name:'富文本编辑器'},
+                    {name:'数据可视化库'},
+                    {name:'移动端组件库'},
+                    {name:'React 图标库'},
+                    {name:'Vue UI 组件库'},
+                    {name:'混用Vue&React'},
+                    {name:'React Hooks 库'},
+                    {name:'中后台管理模版'},
+                    {name:'React UI 组件库'},
+                    {name:'Vue 3 UI 组件库'},
+                    {name:'微信小程序UI组件库'},
+                ]
+            },
+            {
+                name:'实用工具',
+                children:[
+                    {name:'单词练习'},
+                    {name:'简历制作'},
+                    {name:'ChatGPT'},
+                    {name:'调试工具'},
+                    {name:'画图工具'},
+                    {name:'技术博客'},
+                    {name:'前端工具'},
+                    {name:'技术选型'},
+                    {name:'检查依赖包'},
+                    {name:'性能测试工具'},
+                    {name:'Chrome扩展'},
+                    {name:'RSS订阅工具'},
+                    {name:'VS Code 插件'},
+                    {name:'VS Code 主题'},
+                    {name:'在线代码编辑器'},
+                    {name:'WebStorm主题'},
+                    {name:'VS Code 摸鱼插件'},
+                    {name:'JS框架性能基准工具'},
+                ]
+            },
+            {
+                name:'使用技巧',
+                children:[
+                    {name:'Linux 命令'},
+                    {name:'npm 使用技巧'},
+                    {name:'Google 搜索技巧'},
+                    {name:'console 调试技巧'},
+                    {name:'VS Code 快捷方式'},
+                    {
+                        name:'DevTools 调试技巧',
+                        children:[
+                            {name:'DevTools[1]'},
+                            {name:'DevTools[2]'},
+                        ]
+                    },
+                    {
+                        name:'DevTools 实用功能',
+                        children:[
+                            {name:'DevTools[1]'},
+                            {name:'DevTools[2]'},
+                        ]
+                    },
+                    {name:'删除未使用的Css代码'}
+                ]
+            },
+            {
+                name:'学习资源',
+                children:[
+                    {name:'Rust 资源'},
+                    {name:'3D 项目'},
+                    {name:'前端大会'},
+                    {name:'前端书籍'},
+                    {name:'热门项目'},
+                    {name:'Vue 项目'},
+                    {name:'Windows'},
+                    {name:'Awesome'},
+                    {name:'Web3 项目'},
+                    {name:'React 资源'},
+                    {name:'React 项目'},
+                    {name:'低代码项目'},
+                    {name:'Vue3.0项目'},
+                    {name:'Node.js项目'},
+                    {name:'Canvas 项目'},
+                    {name:'前端高仿项目'},
+                    {name:'国外前端博主'},
+                    {name:'前端学习资源'},
+                    {name:'Nuxt 官方项目'},
+                    {name:'跨平台桌面项目'},
+                    {name:'微信小程序项目1'},
+                    {name:'微信小程序项目2'},
+                    {name:'微信小程序项目3'},
+                    {name:'数据结构与算法'},
+                    {name:'TypeScript资源'},
+                    {name:'数据可视化大屏项目'},
+                    {name:'前端框架源码解析项目'},
+                ]
+            }
+        ]
+    }
+]
+
+
+export const Question = [
+    {name:'互联网公司大盘点'},
+    {name:'前端学习路线'},
+    {name:'前端面试准备'},
+    {name:'前端面试题之HTML篇'},
+    {name:'前端面试题之CSS篇'},
+    {name:'前端面试题之JavaScript篇'},
+    {name:'前端面试题之Vue篇'},
+    {name:'前端面试题之React篇'},
+    {name:'前端面试题之性能优化篇'},
+    {name:'前端面试题之前端工程化篇'},
+    {name:'前端面试题之计算机网络篇'},
+    {name:'前端面试题之浏览器原理篇'},
+    {name:'前端面试题之手写代码篇'},
+    {name:'前端面试题之代码输出篇'},
+    {name:'LeetCode 高频题目分类列表'},
+]
+
+export const chartData = [
+    {
+        name:'知识',
+        children:Study
+    },
+    {
+        name:'面试',
+        children:Question
     }
 ]
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -11,184 +11,29 @@ import * as echarts from 'echarts';
 import { useRouter, useRoute } from 'vue-router';
 const router = useRouter();
 const route = useRoute();
+import {chartData} from '/@/router/mds';
 const handleGoRouteMenus = () => {
     router.push({
         path: '/study'
     })
 }
 let chart = ref();
+const computedDataLabel = (data) => {
+    data.map((item) => {
+        if (item.children && item.children.length) computedDataLabel(item.children)
+        else item['value'] = 1;
+    })
+}
+computedDataLabel(chartData);
 const initChart = () => {
     let myChart = echarts.init(chart.value);
-    let data = [
-        {
-            name: '农、林、牧、渔业',
-            children: [
-                {
-                    name: '农业',
-                    value: 43,
-                }, {
-                    name: '林业',
-                    value: 52,
-                }, {
-                    name: '畜牧业',
-                    value: 59,
-                }, {
-                    name: '渔业',
-                    value: 181.5,
-                }, {
-                    name: '农、林、牧、渔服务业',
-                    value: 59,
-                }
-            ]
-        },
-        {
-            name: '采矿业',
-            children: [
-                {
-                    name: '煤炭开采和洗选业',
-                    value: 41.5,
-                }, {
-                    name: '石油和天然气开采业',
-                    value: 49,
-                }
-            ]
-        }
-        , {
-            name: '制造业',
-            children: [
-                {
-                    name: '农副食品加工业',
-                    value: 42.5,
-                }, {
-                    name: '食品制造业',
-                    value: 54,
-                }
-            ]
-        },
-        {
-            name: '电力、热力、燃气及水生产和供应业',
-            children: [
-                {
-                    name: '电力、热力生产和供应业',
-                    value: 148,
-                }, {
-                    name: '燃气生产和供应业',
-                    value: 45.5,
-                }, {
-                    name: '水的生产和供应业',
-                    value: 52.5,
-                }
-            ]
-        },
-        {
-            name: '建筑业',
-            children: [
-                {
-                    name: '房屋建筑业',
-                    value: 41,
-                }, {
-                    name: '土木工程建筑业',
-                    value: 58,
-                }, {
-                    name: '建筑安装业',
-                    value: 55,
-                }, {
-                    name: '建筑装饰和其他建筑业',
-                    value: 17,
-                }
-            ]
-        },
-        {
-            name: '批发和零售业',
-            children: [
-                {
-                    name: '批发业',
-                    value: 125.7,
-                }, {
-                    name: '零售业',
-                    value: 57.5,
-                }
-            ]
-        },
+    let data = chartData;
 
-        {
-            name: '信息传输、软件和信息技术服务业',
-            children: [
-                {
-                    name: '电信、广播电视和卫星传输服务',
-                    value: 82.5,
-                }, {
-                    name: '互联网和相关服务',
-                    value: 50.5,
-                }, {
-                    name: '软件和信息技术服务业',
-                    value: 43,
-                }
-            ]
-        },
-        {
-            name: '金融业',
-            children: [
-                {
-                    name: '货币金融服务',
-                    value: 60.5,
-                }, {
-                    name: '资本市场服务',
-                    value: 83.5,
-                }, {
-                    name: '保险业',
-                    value: 40,
-                }, {
-                    name: '其他金融业',
-                    value: 56,
-                }
-            ]
-        },
-        {
-            name: '房地产业',
-            children: [
-                {
-                    name: '房地产业',
-                    value: 57,
-                }
-            ]
-        },
-        {
-            name: '租赁和商务服务业',
-            children: [
-                {
-                    name: '租赁业',
-                    value: 48,
-                }, {
-                    name: '商务服务业',
-                    value: 49.5,
-                }
-            ]
-        },
-        {
-            name: '科学研究和技术服务业',
-            children: [
-                {
-                    name: '研究和试验发展',
-                    value: 60,
-                }, {
-                    name: '专业技术服务业',
-                    value: 47,
-                }, {
-                    name: '科技推广和应用服务业',
-                    value: 52,
-                }
-            ]
-        }
-
-    ];
     let option = {
         tooltip: {
-            show: true,
-            formatter: (params) => {
-                return params.name + ":" + params.value + '人';
-            },
+            show: false,
         },
+        color:['#FF9797','#FFC1E0','#FFBFFF','#DCB5FF','#66B3FF','#80FFFF','#7AFEC6','#C2FF68','#FF9D6F'],
         series: {
             type: 'sunburst',
             radius: [0, '100%'],
@@ -197,42 +42,9 @@ const initChart = () => {
                 focus: 'ancestor',
             },
             data: data,
-            label: {
-                // rotate: 'tangential',
-                //color: '#fff',
-                formatter: (params) => {
-                    if (params.name.length > 3) {
-                        return params.name.substring(0, 3) + '...';
-                    } else {
-                        return params.name;
-                    }
-                },
-            },
+            label: {},
             levels: [
-                {},
-                {
-                    r0: '15%',
-                    r: '35%',
-                    label: {
-                        rotate: 'tangential',
-                    },
-                },
-                {
-                    r0: '35%',
-                    r: '70%',
-                    label: {
-                        align: 'right',
-                    },
-                },
-                {
-                    r0: '70%',
-                    r: '72%',
-                    label: {
-                        position: 'outside',
-                        padding: 3,
-                        silent: true,
-                    },
-                },
+               
             ],
         },
     };
@@ -250,7 +62,7 @@ onMounted(() => {
 }
 
 .chart {
-    width: 100%;
-    height: 500px;
+    width: 100vw;
+    height: 100vh;
 }
 </style>
