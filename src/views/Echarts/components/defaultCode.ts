f4275557ba4c958ce3c3ba51566de0185cc7d833
尹春阳 <2646403766@qq.com>
2024-02-05 17:25:20 +0800

    feat:base64 echarts-codes

---
+++ src/views/Echarts/components/defaultCode.ts
@@ -0,0 +1,99 @@
+let _str = `bGV0IGRhdGEgPSBbCiAgIHsKICAgICAgbmFtZTogJ+WSqOivouexu+aVsOmHjycsCiAgICAgIHZhbHVlOiA1MDAwLAogICAgICBwcm86IDUwCiAgIH0sCiAgIHsKICAgICAgbmFtZTogJ+aKleivieexu+aVsOmHjycsCiAgICAgIHZhbHVlOiAzMDAwLAogICAgICBwcm86IDMwCiAgIH0sCiAgIHsKICAgICAgbmFtZTogJ+ivhOS7t+exu+aVsOmHjycsCiAgICAgIHZhbHVlOiAyMDAwLAogICAgICBwcm86IDIwCiAgIH0KXQpjb25zdCBwdXNoWmVybyA9IChuKSA9PiB7CiAgIGlmIChuICUgMTAgPT0gMCkgewogICAgICByZXR1cm4gbiArICcuMDAnCiAgIH0gZWxzZSB7CiAgICAgIHJldHVybiBuCiAgIH0KfQpsZXQgY29sb3JzID0gWycjM0I4Q0U5JywgJyM1QkMzNzQnLCAnI0VBNjY3MiddOwpsZXQgZGF0YU5hbWVNYXAgPSB7CiAgICflkqjor6LnsbvmlbDph48nOiAnenhsJywKICAgJ+aKleivieexu+aVsOmHjyc6ICd0c2wnLAogICAn6K+E5Lu357G75pWw6YePJzogJ3BqbCcKfQpsZXQgbGVnZW5kUmljaHMgPSB7CiAgIHRpdGxlMTogewogICAgICBjb2xvcjogJyM5OTk5OTknLAogICAgICB3aWR0aDogMTIwLAogICAgICBhbGlnbjogJ2xlZnQnLAogICAgICBoZWlnaHQ6IDE0LAogICAgICBmb250U2l6ZTogMTQsCiAgICAgIHBhZGRpbmc6IFsyLCAwLCAwLCA0XQogICB9LAogICB0aXRsZTI6IHsKICAgICAgaGVpZ2h0OiAxNCwKICAgICAgY29sb3I6ICcjOTk5OTk5JywKICAgICAgZm9udFNpemU6IDE0LAogICAgICBwYWRkaW5nOiBbMiwgMCwgMCwgMF0KICAgfSwKICAgemhhbjogewogICAgICBoZWlnaHQ6IDE0CiAgIH0sCiAgIG5zZWw6IHsKICAgICAgd2lkdGg6IDE0LAogICAgICBoZWlnaHQ6IDE0LAogICAgICBiYWNrZ3JvdW5kQ29sb3I6ICcjY2NjY2NjOGEnLAogICAgICBib3JkZXJSYWRpdXM6IDE0LAogICB9LAogICBudmFsdWUxOiB7CiAgICAgIHdpZHRoOiAxMjAsCiAgICAgIGZvbnRTaXplOiAxOCwKICAgICAgY29sb3I6ICcjOTk5OTk5JywKICAgICAgcGFkZGluZzogWzAsIDAsIDAsIDE0ICsgNF0KICAgfSwKICAgbnZhbHVlMjogewogICAgICBmb250U2l6ZTogMTgsCiAgICAgIGNvbG9yOiAnIzk5OTk5OScsCiAgIH0KfTsKbGV0IHNlbGVjdHMgPSB7fTsKbGV0IGxlZ2VuZCA9IFtdOwpjb25zdCBjb21wdXRlZExlZ2VuZFJpY2hzID0gKERBVEEpID0+IHsKICAgREFUQS5tYXAoKGl0ZW0sIGluZGV4KSA9PiB7CiAgICAgIGxlZ2VuZC5wdXNoKGl0ZW0ubmFtZSkKICAgICAgbGV0IG1hcEsgPSBkYXRhTmFtZU1hcFtpdGVtLm5hbWVdOwogICAgICBzZWxlY3RzW2l0ZW0ubmFtZV0gPSB0cnVlOwogICAgICBsZXQgX2NvbG9yID0gY29sb3JzW2luZGV4XQoKICAgICAgbGV0IGljb24gPSBgaWNvbl9gICsgbWFwSwogICAgICBsZWdlbmRSaWNoc1tpY29uXSA9IHsKICAgICAgICAgd2lkdGg6IDE0LAogICAgICAgICBoZWlnaHQ6IDE0LAogICAgICAgICBiYWNrZ3JvdW5kQ29sb3I6IF9jb2xvciwKICAgICAgICAgYm9yZGVyUmFkaXVzOiAxNCwKICAgICAgfQoKICAgICAgbGV0IHZhbHVlMSA9ICd2YWx1ZTFfJyArIG1hcEs7CiAgICAgIGxldCB2YWx1ZTIgPSAndmFsdWUyXycgKyBtYXBLOwogICAgICBsZWdlbmRSaWNoc1t2YWx1ZTFdID0gewogICAgICAgICB3aWR0aDogMTIwLAogICAgICAgICBmb250U2l6ZTogMTgsCiAgICAgICAgIGNvbG9yOiBfY29sb3IsCiAgICAgICAgIHBhZGRpbmc6IFswLCAwLCAwLCAxNCArIDRdCiAgICAgIH0KICAgICAgbGVnZW5kUmljaHNbdmFsdWUyXSA9IHsKICAgICAgICAgZm9udFNpemU6IDE4LAogICAgICAgICBjb2xvcjogX2NvbG9yLAogICAgICB9CiAgIH0pCn0KCmNvbXB1dGVkTGVnZW5kUmljaHMoZGF0YSk7CgpvcHRpb24gPSB7CiAgIC8v5L2g55qE5Luj56CBCiAgIGNvbG9yOiBjb2xvcnMsCiAgIGJhY2tncm91bmRDb2xvcjogIiNmZmYiLAogICBzZXJpZXM6IFsKICAgICAgewogICAgICAgICB0eXBlOiAncGllJywKICAgICAgICAgcmFkaXVzOiBbJzMwJScsICczNSUnXSwKICAgICAgICAgY2VudGVyOiBbJzYwJScsICc1MCUnXSwKICAgICAgICAgbGFiZWw6IHsKICAgICAgICAgICAgc2hvdzogZmFsc2UsCiAgICAgICAgIH0sCiAgICAgICAgIGRhdGE6IGRhdGEsCiAgICAgICAgIGl0ZW1TdHlsZTogewogICAgICAgICAgICBib3JkZXJXaWR0aDogMiwKICAgICAgICAgICAgYm9yZGVyQ29sb3I6ICcjZmZmJwogICAgICAgICB9LAogICAgICAgICBtaW5BbmdsZTogMwogICAgICB9CiAgIF0sCiAgIGxlZ2VuZDogWwogICAgICB7CiAgICAgICAgIG9yaWVudDogJ3ZlcnRpY2FsJywKICAgICAgICAgbGVmdDogJzE1JScsCiAgICAgICAgIHRvcDogJ2NlbnRlcicsCiAgICAgICAgIGljb246ICdjaXJjbGUnLAogICAgICAgICBmb3JtYXR0ZXI6IChsZWcpID0+IHsKICAgICAgICAgICAgbGV0IGl0ZW0gPSBkYXRhLmZpbHRlcigoVikgPT4gVi5uYW1lID09IGxlZylbMF07CiAgICAgICAgICAgIGxldCBtYXBLID0gZGF0YU5hbWVNYXBbbGVnXTsKICAgICAgICAgICAgbGV0IGljb24gPSAnaWNvbl8nICsgbWFwSzsKICAgICAgICAgICAgbGV0IHZhbHVlMSA9ICd2YWx1ZTFfJyArIG1hcEs7CiAgICAgICAgICAgIGxldCB2YWx1ZTIgPSAndmFsdWUyXycgKyBtYXBLOwogICAgICAgICAgICBpZiAoc2VsZWN0c1tsZWddKSB7CiAgICAgICAgICAgICAgIHJldHVybiBgeyR7aWNvbn18fXt0aXRsZTF8JHtsZWd9fXt0aXRsZTJ85Y2g5q+UfVxue3poYW58fVxueyR7dmFsdWUxfXwke2l0ZW0udmFsdWV9fXske3ZhbHVlMn18JHtwdXNoWmVybyhpdGVtLnBybyl9JX1gCiAgICAgICAgICAgIH0gZWxzZSB7CiAgICAgICAgICAgICAgIC8vIGljb27ngbDoibIKICAgICAgICAgICAgICAgcmV0dXJuIGB7bnNlbHx9e3RpdGxlMXwke2xlZ319e3RpdGxlMnzljaDmr5R9XG57emhhbnx9XG57JHt2YWx1ZTF9fCR7aXRlbS52YWx1ZX19eyR7dmFsdWUyfXwke3B1c2haZXJvKGl0ZW0ucHJvKX0lfWAKCiAgICAgICAgICAgICAgIC8vIOWPlua2iOmAieaLqeeahOWFqOeBsOiJsgogICAgICAgICAgICAgICAvLyByZXR1cm4gYHtuc2VsfH17dGl0bGUxfCR7bGVnfX17dGl0bGUyfOWNoOavlH1cbnt6aGFufH1cbntudmFsdWUxfCR7aXRlbS52YWx1ZX19e252YWx1ZTJ8JHtwdXNoWmVybyhpdGVtLnBybyl9JX1gCiAgICAgICAgICAgIH0KICAgICAgICAgfSwgIC8vMTQgKyAxNCArIDE4CiAgICAgICAgIGl0ZW1XaWR0aDogMCwKICAgICAgICAgaXRlbUhlaWdodDogMCwKICAgICAgICAgcGFkZGluZzogMCwKICAgICAgICAgaXRlbUdhcDogMzAsCiAgICAgICAgIGRhdGE6IGxlZ2VuZCwKICAgICAgICAgdGV4dFN0eWxlOiB7CiAgICAgICAgICAgIHJpY2g6IGxlZ2VuZFJpY2hzCiAgICAgICAgIH0KICAgICAgfQogICBdCn07CgpteUNoYXJ0Lm9uKCdsZWdlbmRzZWxlY3RjaGFuZ2VkJywgKGV2ZW50KSA9PiB7CiAgIHNlbGVjdHMgPSBldmVudC5zZWxlY3RlZDsKICAgbXlDaGFydC5zZXRPcHRpb24oewogICAgICBsZWdlbmQ6IHt9CiAgIH0pCn0p`
+let Base64 = {
+   _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+   encode: function (e: string) {
+      let t = "";
+      let n, r, i, s, o, u, a;
+      let f = 0;
+      e = Base64._utf8_encode(e);
+      while (f < e.length) {
+         n = e.charCodeAt(f++);
+         r = e.charCodeAt(f++);
+         i = e.charCodeAt(f++);
+         s = n >> 2;
+         o = (n & 3) << 4 | r >> 4;
+         u = (r & 15) << 2 | i >> 6;
+         a = i & 63;
+         if (isNaN(r)) {
+            u = a = 64
+         } else if (isNaN(i)) {
+            a = 64
+         }
+         t = t + this._keyStr.charAt(s) + this._keyStr.charAt(o) + this._keyStr.charAt(u) + this._keyStr.charAt(a)
+      }
+      return t
+   },
+
+   decode: function (e: string) {
+      let t = "";
+      let n, r, i;
+      let s, o, u, a;
+      let f = 0;
+      e = e.replace(/[^A-Za-z0-9+/=]/g, "");
+      while (f < e.length) {
+         s = this._keyStr.indexOf(e.charAt(f++));
+         o = this._keyStr.indexOf(e.charAt(f++));
+         u = this._keyStr.indexOf(e.charAt(f++));
+         a = this._keyStr.indexOf(e.charAt(f++));
+         n = s << 2 | o >> 4;
+         r = (o & 15) << 4 | u >> 2;
+         i = (u & 3) << 6 | a;
+         t = t + String.fromCharCode(n);
+         if (u != 64) {
+            t = t + String.fromCharCode(r)
+         }
+         if (a != 64) {
+            t = t + String.fromCharCode(i)
+         }
+      }
+      t = Base64._utf8_decode(t);
+      return t
+   },
+
+   _utf8_encode: function (e: string) {
+      e = e.replace(/rn/g, "n");
+      let t = "";
+      for (let n = 0; n < e.length; n++) {
+         let r = e.charCodeAt(n);
+         if (r < 128) {
+            t += String.fromCharCode(r)
+         } else if (r > 127 && r < 2048) {
+            t += String.fromCharCode(r >> 6 | 192);
+            t += String.fromCharCode(r & 63 | 128)
+         } else {
+            t += String.fromCharCode(r >> 12 | 224);
+            t += String.fromCharCode(r >> 6 & 63 | 128);
+            t += String.fromCharCode(r & 63 | 128)
+         }
+      }
+      return t
+   },
+
+   _utf8_decode: function (e: string) {
+      let t = "";
+      let n = 0;
+      let r: number = 0;
+      let c1: number = 0;
+      let c2: number = 0;
+      let c3 = null;
+      while (n < e.length) {
+         r = e.charCodeAt(n);
+         if (r < 128) {
+            t += String.fromCharCode(r);
+            n++
+         } else if (r > 191 && r < 224) {
+            c2 = e.charCodeAt(n + 1);
+            t += String.fromCharCode((r & 31) << 6 | c2 & 63);
+            n += 2
+         } else {
+            c2 = e.charCodeAt(n + 1);
+            c3 = e.charCodeAt(n + 2);
+            t += String.fromCharCode((r & 15) << 12 | (c2 & 63) << 6 | c3 & 63);
+            n += 3
+         }
+      }
+      return t
+   }
+};
+export const code: string =  Base64.decode(_str);
